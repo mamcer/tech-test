ea2033264d6dd69f2e60e23eb3a3911ece70a21f
Mario Moreno <m4mc3r@gmail.com>
2022-05-01 09:38:27 -0300

    Add some refactor to palindrome test

--- palindrome_test.go
+++ palindrome_test.go
@@ -2,38 +2,36 @@ package palindrome
 
 import "testing"
 
+const palindromeExample = "racecar"
+const notPalindromeExample = "mario"
+
+var validateEquivalence = func(t *testing.T, got, want bool) {
+	t.Helper()
+	if got != want {
+		t.Errorf("Got:%v, expect:%v", got, want)
+	}
+}
+
 func TestIsPalindromeFor(t *testing.T) {
 	t.Run("is palindrome", func(t *testing.T) {
-		got := IsPalindromeFor("racecar")
-		want := true
-		if got != want {
-			t.Errorf("Got:%v, expect:%v", got, want)
-		}
+		got := IsPalindromeFor(palindromeExample)
+		validateEquivalence(t, got, true)
 	})
 
 	t.Run("is not palindrome", func(t *testing.T) {
-		got := IsPalindromeFor("mario")
-		want := false
-		if got != want {
-			t.Errorf("Got:%v, expect:%v", got, want)
-		}
+		got := IsPalindromeFor(notPalindromeExample)
+		validateEquivalence(t, got, false)
 	})
 }
 
 func TestIsPalindromeSort(t *testing.T) {
 	t.Run("is palindrome", func(t *testing.T) {
-		got := IsPalindromeSort("racecar")
-		want := true
-		if got != want {
-			t.Errorf("Got:%v, expect:%v", got, want)
-		}
+		got := IsPalindromeSort(palindromeExample)
+		validateEquivalence(t, got, true)
 	})
 
 	t.Run("is not palindrome", func(t *testing.T) {
-		got := IsPalindromeSort("mario")
-		want := false
-		if got != want {
-			t.Errorf("Got:%v, expect:%v", got, want)
-		}
+		got := IsPalindromeSort(notPalindromeExample)
+		validateEquivalence(t, got, false)
 	})
 }
