0a8bbbf75833e6a436fe22f09bfb3fd70b9b475d
Mario Moreno <mario.moreno@mercadolibre.com>
2022-05-01 16:27:59 -0300

    Add map option

--- two-items/two.go
+++ two-items/two.go
@@ -33,3 +33,34 @@ func TwoItemsSort(items []int, value int) bool {
 
 	return false
 }
+
+func TwoItemsMap(items []int, value int) bool {
+	// sort.Ints(items)
+	var here map[int]int = make(map[int]int)
+
+	for i := 0; i < len(items); i++ {
+		// if items[i] > value {
+		// 	break
+		// }
+		me := items[i]
+
+		if v, ok := here[me]; ok {
+			if v == 0 {
+				return true
+			}
+		}
+
+		here[me] = 1
+
+		want := value - me
+		if v, ok := here[want]; ok {
+			if v == 1 {
+				return true
+			}
+		}
+
+		here[want] = 0
+	}
+
+	return false
+}
--- two-items/two_test.go
+++ two-items/two_test.go
@@ -46,3 +46,22 @@ func TestTwoItemsSort(t *testing.T) {
 		validateEquivalence(t, got, false)
 	})
 }
+
+func TestTwoItemsMap(t *testing.T) {
+
+	t.Run("two items success", func(t *testing.T) {
+		got := TwoItemsMap([]int{7, 1, 3, 2, 6}, 5)
+
+		validateEquivalence(t, got, true)
+	})
+
+	t.Run("two items fail", func(t *testing.T) {
+		got := TwoItemsMap([]int{1, 2, 4, 6, 10, 12, 13, 64}, 100)
+		validateEquivalence(t, got, false)
+	})
+
+	t.Run("empty array fail", func(t *testing.T) {
+		got := TwoItemsMap([]int{}, 10)
+		validateEquivalence(t, got, false)
+	})
+}
