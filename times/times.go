2e7f4541fe3122f1439a393ab371bae276821325
Mario Moreno <mario.moreno@mercadolibre.com>
2022-05-02 08:20:09 -0300

    Add map solution option

--- times/times.go
+++ times/times.go
@@ -10,3 +10,16 @@ func StringTimes(s string, v string) int {
 
 	return c
 }
+
+func StringTimesMap(s string, v string) (map[string]int, int) {
+	var m map[string]int = make(map[string]int)
+	for i := 0; i < len(s); i++ {
+		if _, ok := m[string(s[i])]; ok {
+			m[string(s[i])] += 1
+		} else {
+			m[string(s[i])] = 1
+		}
+	}
+
+	return m, m[v]
+}
--- times/times_test.go
+++ times/times_test.go
@@ -28,3 +28,23 @@ func TestStringTimes(t *testing.T) {
 		validateEquivalence(t, got, 0)
 	})
 }
+
+func TestStringTimesMap(t *testing.T) {
+	t.Run("string exists", func(t *testing.T) {
+		_, got := StringTimesMap("alabanza", "a")
+
+		validateEquivalence(t, got, 4)
+	})
+
+	t.Run("string does not exists", func(t *testing.T) {
+		_, got := StringTimesMap("alabanza", "c")
+
+		validateEquivalence(t, got, 0)
+	})
+
+	t.Run("empty string should return zero", func(t *testing.T) {
+		_, got := StringTimesMap("", "a")
+
+		validateEquivalence(t, got, 0)
+	})
+}
