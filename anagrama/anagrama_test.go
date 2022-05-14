bad51f416b416961579eb032cf0c8f75b75e704a
Mario Moreno <m4mc3r@gmail.com>
2022-05-14 17:04:24 -0300

    Add an anagrama map solution

--- anagrama/anagrama_test.go
+++ anagrama/anagrama_test.go
@@ -10,29 +10,55 @@ var validateEquivalence = func(t *testing.T, got, want bool) {
 }
 
 func TestIsAnagramaSort(t *testing.T) {
-
-	t.Run("is palindrome", func(t *testing.T) {
+	t.Run("is anagrama", func(t *testing.T) {
 		got := IsAnagramaSort("mario", "omira")
 		validateEquivalence(t, got, true)
 	})
 
-	t.Run("is not palindrome", func(t *testing.T) {
+	t.Run("is not anagrama", func(t *testing.T) {
 		got := IsAnagramaSort("mario", "orama")
 		validateEquivalence(t, got, false)
 	})
 
-	t.Run("empty is false", func(t *testing.T) {
+	t.Run("non empty vs empty should be false", func(t *testing.T) {
 		got := IsAnagramaSort("mario", "")
 		validateEquivalence(t, got, false)
 	})
 
-	t.Run("empty is false", func(t *testing.T) {
+	t.Run("empty vs non empty should be false", func(t *testing.T) {
 		got := IsAnagramaSort("", "mario")
 		validateEquivalence(t, got, false)
 	})
 
-	t.Run("is palindrome", func(t *testing.T) {
+	t.Run("also even with spaces should be true", func(t *testing.T) {
 		got := IsAnagramaSort("acuerdo japones", "ecuador esponja")
+		validateEquivalence(t, got, true)
+	})
+}
+
+func TestIsAnagramaMap(t *testing.T) {
+	t.Run("is anagrama", func(t *testing.T) {
+		got := IsAnagramaMap("mario", "omira")
+		validateEquivalence(t, got, true)
+	})
+
+	t.Run("is not anagrama", func(t *testing.T) {
+		got := IsAnagramaMap("mario", "orama")
+		validateEquivalence(t, got, false)
+	})
+
+	t.Run("non empty vs empty should be false", func(t *testing.T) {
+		got := IsAnagramaMap("mario", "")
+		validateEquivalence(t, got, false)
+	})
+
+	t.Run("empty vs non empty should be false", func(t *testing.T) {
+		got := IsAnagramaMap("", "mario")
+		validateEquivalence(t, got, false)
+	})
+
+	t.Run("also even with spaces should be true", func(t *testing.T) {
+		got := IsAnagramaMap("acuerdo japones", "ecuador esponja")
 		validateEquivalence(t, got, true)
 	})
 }
