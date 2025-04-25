0b7944a757de1102d21af8da02af94cffb64b33a
Mario Moreno <m4mc3r@gmail.com>
2025-04-24 21:50:06 -0300

    Add initial compactador netcore version

--- dotnet/compactor/src/Compactor.Cli/Program.cs
+++ dotnet/compactor/src/Compactor.Cli/Program.cs
@@ -6,6 +6,35 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var word = "aaaaabbbadddcc";
+        Console.WriteLine($"compactor result for: {word} is {Compactor(word)}");
+    }
+
+    public static string Compactor(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return string.Empty;
+        }
+
+        var current = input[0];
+        var result = string.Empty;
+        var count = 1;
+        for (int i = 1; i < input.Length; i++)
+        {
+            if (input[i] == current)
+            {
+                count++;
+                continue;
+            }
+            else
+            {
+                result += current + count.ToString();
+                current = input[i];
+                count = 1;
+            }
+        }
+        result += current + count.ToString();
+        return result;
     }
 }
