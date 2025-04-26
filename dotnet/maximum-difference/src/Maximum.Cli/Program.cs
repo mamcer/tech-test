3bd76e651e6d445166c563f2d343151618032b93
Mario Moreno <m4mc3r@gmail.com>
2025-04-25 22:49:59 -0300

    Add initial maximum difference

--- dotnet/maximum-difference/src/Maximum.Cli/Program.cs
+++ dotnet/maximum-difference/src/Maximum.Cli/Program.cs
@@ -6,6 +6,28 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[] input = { 15, 3, 6, 10 };
+        Console.WriteLine("the maximum difference is: " + MaximumDifference(input));
+    }
+
+    public static int MaximumDifference(int[] arr)
+    {
+        int maxDiff = -1;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            for (int j = i + 1; j < arr.Length; j++)
+            {
+                if (arr[j] > arr[i])
+                {
+                    if ( (arr[j] - arr[i]) > maxDiff)
+                    {
+                        maxDiff = arr[j] - arr[i];
+                    }
+                }
+            }
+        }
+
+        return maxDiff;
     }
 }
