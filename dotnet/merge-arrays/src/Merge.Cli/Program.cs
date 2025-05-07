a4cbe758836274f6d41ee28f1262e066fd65589a
Mario Moreno <m4mc3r@gmail.com>
2025-05-06 23:34:01 -0300

    Add some name change

--- dotnet/merge-arrays/src/Merge.Cli/Program.cs
+++ dotnet/merge-arrays/src/Merge.Cli/Program.cs
@@ -4,9 +4,39 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        var array1 = new[] { 1, 3, 5, 7, 9 };
-        var array2 = new[] { 2, 4, 6, 8, 10 };
-        var mergedArray = MergeArrays(array1, array2);
+        var array01 = new[] { 1, 3, 5, 7, 9 };
+        var array02 = new[] { 2, 4, 6, 8, 10 };
+        var mergedArray = MergeArrays(array01, array02);
         Console.WriteLine("Merged Array: " + string.Join(", ", mergedArray));
+    }
+
+    private static int[] MergeArrays(int[] array01, int[] array02)
+    {
+        int[] mergedArray = new int[array01.Length + array02.Length];
+        int i = 0, j = 0, k = 0;
+
+        while (i < array01.Length && j < array02.Length)
+        {
+            if (array01[i] < array02[j])
+            {
+                mergedArray[k++] = array01[i++];
+            }
+            else
+            {
+                mergedArray[k++] = array02[j++];
+            }
+        }
+
+        while (i < array01.Length)
+        {
+            mergedArray[k++] = array01[i++];
+        }
+
+        while (j < array02.Length)
+        {
+            mergedArray[k++] = array02[j++];
+        }
+
+        return mergedArray;
     }
 }
