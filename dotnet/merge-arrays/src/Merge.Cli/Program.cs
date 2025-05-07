eeb1b0290f913cfb508a4d9e2c97c95724f07ed2
Mario Moreno <m4mc3r@gmail.com>
2025-05-06 23:30:41 -0300

    Add array comparison method

--- dotnet/merge-arrays/src/Merge.Cli/Program.cs
+++ dotnet/merge-arrays/src/Merge.Cli/Program.cs
@@ -4,6 +4,9 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var array1 = new[] { 1, 3, 5, 7, 9 };
+        var array2 = new[] { 2, 4, 6, 8, 10 };
+        var mergedArray = MergeArrays(array1, array2);
+        Console.WriteLine("Merged Array: " + string.Join(", ", mergedArray));
     }
 }
