818d73e1e172527624e57d4108aadbcf76d087ef
Mario Moreno <m4mc3r@gmail.com>
2025-05-10 23:40:35 -0300

    Add first working version

--- dotnet/palindrome/src/Palindrome.Cli/Program.cs
+++ dotnet/palindrome/src/Palindrome.Cli/Program.cs
@@ -15,4 +15,22 @@ public class Program
             Console.WriteLine($"'{word}' is not a palindrome.");
         }
     }
+
+    public static bool IsPalindrome(string word)
+    {
+        int left = 0;
+        int right = word.Length - 1;
+
+        while (left < right)
+        {
+            if (word[left] != word[right])
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
 }
