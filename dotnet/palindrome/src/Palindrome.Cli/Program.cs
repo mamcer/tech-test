99d2663107ebc520dea6dfe51df2c70aacc55e58
Mario Moreno <m4mc3r@gmail.com>
2025-05-10 23:38:46 -0300

    Add initial solution structure

--- dotnet/palindrome/src/Palindrome.Cli/Program.cs
+++ dotnet/palindrome/src/Palindrome.Cli/Program.cs
@@ -4,6 +4,15 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var word = "racecar";
+        var isPalindrome = IsPalindrome(word);
+        if (isPalindrome)
+        {
+            Console.WriteLine($"'{word}' is a palindrome.");
+        }
+        else
+        {
+            Console.WriteLine($"'{word}' is not a palindrome.");
+        }
     }
 }
