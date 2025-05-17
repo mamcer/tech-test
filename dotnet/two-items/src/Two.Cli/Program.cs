36eb645aaf49c2463ec340c0f0271f33fe090b2c
Mario Moreno <m4mc3r@gmail.com>
2025-05-17 19:24:54 -0300

    Add a little refactor

--- dotnet/two-items/src/Two.Cli/Program.cs
+++ dotnet/two-items/src/Two.Cli/Program.cs
@@ -8,17 +8,22 @@ public class Program
         var numbers = new[] { 2, 7, 11, 15 };
         Console.WriteLine("The array has two numbers that sum to " + target + ": " + HasTwoSum(numbers, target));
     }
-    
-        public static bool HasTwoSum(int[] numbers, int target)
+
+    public static bool HasTwoSum(int[] numbers, int target)
     {
         var seen = new HashSet<int>();
         foreach (var num in numbers)
         {
             int complement = target - num;
             if (seen.Contains(complement))
+            {
                 return true;
+
+            }
+
             seen.Add(num);
         }
+
         return false;
     }
 }
