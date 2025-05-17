221ab00295b4e6947e8bc1c02205803b5811e017
Mario Moreno <m4mc3r@gmail.com>
2025-05-16 23:03:09 -0300

    Add times implementation

--- dotnet/times/src/Times.Cli/Program.cs
+++ dotnet/times/src/Times.Cli/Program.cs
@@ -3,7 +3,23 @@
 public class Program
 {
     public static void Main()
-    { 
-        
+    {
+        char value = 'a';
+        string word = "aabbccddaaeeffgg";
+        Console.WriteLine($"{value} is appearing {Times(value, word)} times in the word '{word}'");
+    }
+    
+    public static int Times(char value, string word)
+    {
+        int count = 0;
+        foreach (char c in word)
+        {
+            if (c == value)
+            {
+                count++;
+            }
+        }
+ 
+        return count;
     }
 }
