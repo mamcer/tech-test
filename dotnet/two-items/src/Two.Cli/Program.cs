e0f8204b12010eed1b47717cf9bda3c3f3f14e51
Mario Moreno <m4mc3r@gmail.com>
2025-05-17 19:22:52 -0300

    Add Has two items method

--- dotnet/two-items/src/Two.Cli/Program.cs
+++ dotnet/two-items/src/Two.Cli/Program.cs
@@ -3,7 +3,22 @@
 public class Program
 {
     public static void Main()
-    { 
-        Console.WriteLine("Hello, World!");
+    {
+        var target = 9;
+        var numbers = new[] { 2, 7, 11, 15 };
+        Console.WriteLine("The array has two numbers that sum to " + target + ": " + HasTwoSum(numbers, target));
+    }
+    
+        public static bool HasTwoSum(int[] numbers, int target)
+    {
+        var seen = new HashSet<int>();
+        foreach (var num in numbers)
+        {
+            int complement = target - num;
+            if (seen.Contains(complement))
+                return true;
+            seen.Add(num);
+        }
+        return false;
     }
 }
