c4381524e5ba67ff44e7b903365f8b03bd3ef71f
Mario Moreno <m4mc3r@gmail.com>
2025-05-17 19:26:30 -0300

    Change input

--- dotnet/two-items/src/Two.Cli/Program.cs
+++ dotnet/two-items/src/Two.Cli/Program.cs
@@ -5,7 +5,7 @@ public class Program
     public static void Main()
     {
         var target = 9;
-        var numbers = new[] { 2, 7, 11, 15 };
+        var numbers = new[] { 2, 7, 5, 15 };
         Console.WriteLine("The array has two numbers that sum to " + target + ": " + HasTwoSum(numbers, target));
     }
 
