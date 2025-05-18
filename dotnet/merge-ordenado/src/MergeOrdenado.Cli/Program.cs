c7debbc7b462217905333489ed841950bf87197f
Mario Moreno <m4mc3r@gmail.com>
2025-05-18 15:30:26 -0300

    Fix merge result console write

--- dotnet/merge-ordenado/src/MergeOrdenado.Cli/Program.cs
+++ dotnet/merge-ordenado/src/MergeOrdenado.Cli/Program.cs
@@ -5,11 +5,11 @@ public class Program
     public static void Main(string[] args)
     {
         var array01 = new int[] { 1, 3, 5, 7, 9 };
-        var array02 = new int[] { 2, 4, 6, 8, 10 };
+        var array02 = new int[] { 2, 3, 6, 8, 10 };
 
         Console.WriteLine("array 01: " + string.Join(", ", array01));
-        Console.WriteLine("array 02: " + string.Join(", ", array01));
-        Console.WriteLine($"merge ordenado result: {MergeOrdenado(array01, array02)}");
+        Console.WriteLine("array 02: " + string.Join(", ", array02));
+        Console.WriteLine($"merge ordenado result: {string.Join(", ", MergeOrdenado(array01, array02))}");
     }
 
     public static int[] MergeOrdenado(int[] array01, int[] array02)
