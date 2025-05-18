a9aad3fe68f3f5cf7e1c65113285fb5457f77b3c
Mario Moreno <m4mc3r@gmail.com>
2025-05-18 15:25:42 -0300

    Add merge ordenado main structure

--- dotnet/merge-ordenado/src/MergeOrdenado.Cli/Program.cs
+++ dotnet/merge-ordenado/src/MergeOrdenado.Cli/Program.cs
@@ -3,7 +3,12 @@
 public class Program
 {
     public static void Main(string[] args)
-    { 
-        Console.WriteLine("Hello, World!");
+    {
+        var array01 = new int[] { 1, 3, 5, 7, 9 };
+        var array02 = new int[] { 2, 4, 6, 8, 10 };
+
+        Console.WriteLine("array 01: " + string.Join(", ", array01));
+        Console.WriteLine("array 02: " + string.Join(", ", array01));
+        Console.WriteLine($"merge ordenado result: {MergeOrdenado(array01, array02)}");
     }
 }
