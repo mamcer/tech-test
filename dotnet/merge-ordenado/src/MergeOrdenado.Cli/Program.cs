4ce89758ed556dbe37bea10547a9afeb012cbc68
Mario Moreno <m4mc3r@gmail.com>
2025-05-18 15:26:44 -0300

    Add Merge Ordenado implementation

--- dotnet/merge-ordenado/src/MergeOrdenado.Cli/Program.cs
+++ dotnet/merge-ordenado/src/MergeOrdenado.Cli/Program.cs
@@ -11,4 +11,36 @@ public class Program
         Console.WriteLine("array 02: " + string.Join(", ", array01));
         Console.WriteLine($"merge ordenado result: {MergeOrdenado(array01, array02)}");
     }
+
+    public static int[] MergeOrdenado(int[] array01, int[] array02)
+    {
+        var result = new int[array01.Length + array02.Length];
+        var i = 0;
+        var j = 0;
+        var k = 0;
+
+        while (i < array01.Length && j < array02.Length)
+        {
+            if (array01[i] < array02[j])
+            {
+                result[k++] = array01[i++];
+            }
+            else
+            {
+                result[k++] = array02[j++];
+            }
+        }
+
+        while (i < array01.Length)
+        {
+            result[k++] = array01[i++];
+        }
+
+        while (j < array02.Length)
+        {
+            result[k++] = array02[j++];
+        }
+
+        return result;
+    }
 }
